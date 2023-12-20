14569788547528f231fa552b3091a91d748c09f4
Maksimovic Ivana <maksimovic.ivana@protonmail.com>
2023-12-20 14:13:33 +0100

    changed naming

--- example-project/multi-tenancy-example/src/main/kotlin/com/example/domain/repository/FruitsRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.domain.repository
-
-import com.example.domain.FruitsEntity
-import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase
-import jakarta.enterprise.context.ApplicationScoped
-
-
-@ApplicationScoped
-class FruitsRepository : PanacheRepositoryBase<FruitsEntity, Long>
--- example-project/multi-tenancy-example/src/main/resources/schema/V1.0.0__tenant_setup.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists fruits
-(
-    id   bigserial primary key,
-    name character varying(255)
-);
-
-insert into fruits(id, name)
-VALUES (nextval('fruits_id_seq'), 'Apple');
